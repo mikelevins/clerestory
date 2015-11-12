c142267ed4c2995f794433f6cd8d9e470b0a1c41
mikel evins <mevins@me.com>
2015-11-12 15:47:16 -0600

    initial import

---
+++ src/clerestory/core.clj
@@ -0,0 +1,27 @@
+(ns clerestory.core
+  (:gen-class)
+  (:use seesaw.core seesaw.font)
+  (:import (org.fife.ui.rsyntaxtextarea RSyntaxTextArea SyntaxConstants
+                                        TokenMakerFactory)	
+           (org.fife.ui.rtextarea RTextScrollPane)))
+
+(defn make-text-area []
+  (let [area (RSyntaxTextArea.)]
+    (doto area
+      (.setAnimateBracketMatching false)
+      (.setBracketMatchingEnabled false)
+      (.setAutoIndentEnabled false)
+      (.setAntiAliasingEnabled true)
+      (.setLineWrap false)
+      (.setFont (font :name "Menlo" :size 16)))
+    area))
+
+(defn make-listener []
+  (let [win (frame :title "Listener" :minimum-size [512 :by 240])
+        ed (make-text-area)]
+    (.add win ed)
+    (-> win pack! show!)
+    win))
+
+(defn -main [& args]
+  (make-listener))
